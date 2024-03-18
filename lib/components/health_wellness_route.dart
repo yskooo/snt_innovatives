2bf2a5d2de5316aad1bfe6ffa958a1f290b09e5b
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2024-03-18 20:56:55 +0800

    checkup: ui skeletal

--- lib/components/health_wellness_route.dart
+++ lib/components/health_wellness_route.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_supabase/widgets/card_route_forms.dart'; // Import the file for the new page
 
 class HealthWellnessRoute extends StatelessWidget {
-  const HealthWellnessRoute({super.key});
+  const HealthWellnessRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,31 +13,31 @@ class HealthWellnessRoute extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
-                height: 150, // Adjust height as needed
-                color: Colors.tealAccent.shade700, // Adjust color as needed
+                height: 100, // Adjust height as needed
+                color: Colors.white, // Adjust color as needed
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  padding: const EdgeInsets.all(15.0),
                   child: Row(
                     children: [
                       IconButton(
-                        icon: Icon(Icons.arrow_back),
+                        icon: const Icon(Icons.arrow_back),
                         onPressed: () {
                           Navigator.pop(context);
                         },
                       ),
-                      Expanded(
+                      const Expanded(
                         child: Center(
                           child: Text(
                             'Checkup',
                             style: TextStyle(
-                              fontSize: 20,
+                              fontSize: 25,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
                         ),
                       ),
-                      CircleAvatar(
-                        backgroundImage: AssetImage('assets/avatar.png'), // Adjust image as needed
+                      const CircleAvatar(
+                        backgroundImage: NetworkImage('https://avatars.githubusercontent.com/u/90546802?v=4'), // Adjust image as needed
                       ),
                     ],
                   ),
@@ -44,14 +45,51 @@ class HealthWellnessRoute extends StatelessWidget {
               ),
               Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(20.0),
                   child: GridView.count(
                     crossAxisCount: 2, // 2 cards per row, adjust as needed
+                    crossAxisSpacing: 10, // Add spacing between the cards horizontally
+                    mainAxisSpacing: 10, // Add spacing between the cards vertically
                     children: List.generate(
                       8,
-                          (index) => Card(
-                        child: Center(
-                          child: Text('Card ${index + 1}'),
+                          (index) => GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                              builder: (context) => CardRouteForms(unitNumber: index + 1),
+                            ),
+                          );
+                        },
+                        child: Card(
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const SizedBox(height: 10),
+                              const Center(
+                                child: Icon(
+                                  Icons.local_hospital_rounded,
+                                  color: Colors.green,
+                                  size: 45.0,
+                                ),
+                              ),
+                              const SizedBox(height: 10), // Adjust the height of the space between icon and text
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 10), // Adjust horizontal padding for the text
+                                child: Text(
+                                  'Rural Health \nUnit ${index + 1}',
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
